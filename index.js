4e9cd81518039fd3109890983d324c9c81cc8836
Harmony <harmonymukolwe@gmail.com>
2024-07-02 13:18:10 +0300

    Implements array find method

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(arrayOfObjects) {
+  const arr = arrayOfObjects.find((array, i) => {
+    return array.result === "W";
+  });
+
+  if (arr) {
+    return arr.year;
+  } else {
+    return arr;
+  }
+}
+
+superbowlWin(record);
